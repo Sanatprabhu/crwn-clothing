0c81efba645744e39e38111dcb8ea81f3849e1b1
Sanat Prabhu <sanat_prabhu@yahoo.com>
2021-07-02 13:24:55 +0530

    made forms, buttons and customized them

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
+
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
 import Header from "./components/header/header.component";
+
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 
 function App() {
   return (
@@ -14,7 +18,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
 
-        <Route  path="/shop" component={ShopPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
+
+
       </Switch>
     </div>
   );
